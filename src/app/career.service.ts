46bb0434df0ee946685da5b71ab3fb5b1ba85cfe
David Aeschlimann <david.aeschlimann96@gmail.com>
2022-09-05 16:10:16 +0200

    Added data for experiences and educations, added career section and projects section

--- src/app/career.service.ts
+++ src/app/career.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Career } from './career';
-import * as exp from '../assets/data/experience.json';
-import * as edu from '../assets/data/education.json';
+import * as exp from '../assets/data/experiences.json';
+import * as edu from '../assets/data/educations.json';
 
 @Injectable({
   providedIn: 'root'
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../projects.service';
+import { Project } from '../project';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public projects: Array<Project> = [];
+
+  constructor(
+    private _projectService: ProjectsService
+  ) { }
 
   ngOnInit(): void {
+    this.projects = this._projectService.getPro();
   }
 
 }
