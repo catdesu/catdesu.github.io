3184eba9a97d4cb3c9105ae14380b519ce453696
Cat Sama <david.aeschlimann96@gmail.com>
2024-02-05 10:45:57 +0100

    Remove unused import statements

--- src/app/components/base/base.component.ts
+++ src/app/components/base/base.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-base',
