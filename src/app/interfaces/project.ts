f118b1944e8e93e17eefc5bdb6bd1558bae5d367
catdesu <david.aeschlimann96@gmail.com>
2024-02-07 08:47:35 +0100

    Add a project and repository button

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -2,7 +2,8 @@ export interface Project {
     id: string,
     name: string,
     image: string,
-    url: string,
+    url?: string,
+    repository?: string,
     pro: boolean,
     description: string
 }
